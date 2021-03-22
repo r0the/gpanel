9bed968d2d8a1702415c2434692ab4359a7c9b08
Stefan Rothe <stefan.rothe@gymkirchenfeld.ch>
2021-03-22 07:21:04 +0100

    Rect: Renamed properties x -> left, y -> bottom

--- gpanel/Rect.py
+++ gpanel/Rect.py
@@ -1,13 +1,13 @@
 class Rect:
-    def __init__(self, x, y, width, height):
-        self._x = x
-        self._y = y
+    def __init__(self, left, bottom, width, height):
+        self._left = left
+        self._bottom = bottom
         self._width = width
         self._height = height
 
     def __repr__(self):
-        return "<%s (x: %s, y: %s, width: %s, height: %s)>" % (
-            self.__class__.__name__, self._x, self._y, self._width, self._height)
+        return "<%s (left: %s, bottom: %s, width: %s, height: %s)>" % (
+            self.__class__.__name__, self._left, self._bottom, self._width, self._height)
 
     def _get_width(self):
         return self._width
@@ -26,49 +26,49 @@ class Rect:
     height = property(_get_height, _set_height)
     
     def _get_left(self):
-        return self._x
+        return self._left
 
     def _set_left(self, left):
-        self._x = left
+        self._left = left
 
     left = property(_get_left, _set_left)
 
     def _get_right(self):
-        return self._x + self._width
+        return self._left + self._width
 
     def _set_right(self, right):
-        self._x = right - self._width
+        self._left = right - self._width
 
     right = property(_get_right, _set_right)
 
-    def _get_top(self):
-        return self._y + self._height
-
-    def _set_top(self, top):
-        self._y = top - self._height
-
-    top = property(_get_top, _set_top)
-
     def _get_bottom(self):
-        return self._y
+        return self._bottom
 
     def _set_bottom(self, bottom):
-        self._y = bottom
+        self._bottom = bottom
 
     bottom = property(_get_bottom, _set_bottom)
 
+    def _get_top(self):
+        return self._bottom + self._height
+
+    def _set_top(self, top):
+        self._bottom = top - self._height
+
+    top = property(_get_top, _set_top)
+
     def _get_x(self):
-        return self._x + self._width / 2
+        return self._left + self._width / 2
 
     def _set_x(self, x):
-        self._x = x - self._width / 2
+        self._left = x - self._width / 2
 
     x = property(_get_x, _set_x)
 
     def _get_y(self):
-        return self._y + self._height / 2
+        return self._bottom + self._height / 2
 
     def _set_y(self, x):
-        self._y = y - self._height / 2
+        self._bottom = y - self._height / 2
 
     y = property(_get_y, _set_y)
